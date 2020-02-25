8b20f68e87f450cd2f50749915554b19868451c8
Sayan <shayan1337@gmail.com>
2020-02-25 20:08:24 +0530

    Add configuration object for reading configs from application.conf file

---
+++ src/main/scala/com/sayan/sns/config/Configuration.scala
@@ -0,0 +1,27 @@
+package com.sayan.sns.config
+
+import com.typesafe.config.ConfigFactory
+import util.Try
+
+/**
+ * Holds service configuration settings.
+ */
+trait Configuration {
+
+  val config = ConfigFactory.load()
+  
+  lazy val serviceHost = Try(config.getString("service.host")).getOrElse("localhost")
+
+  lazy val servicePort = Try(config.getInt("service.port")).getOrElse(8080)
+
+  lazy val dbHost = Try(config.getString("db.host")).getOrElse("localhost")
+
+  lazy val dbPort = Try(config.getInt("db.port")).getOrElse(3306)
+
+  lazy val dbName = Try(config.getString("db.name")).getOrElse("rest")
+
+  lazy val dbUser = Try(config.getString("db.user")).toOption.orNull
+
+  lazy val dbPassword = Try(config.getString("db.password")).toOption.orNull
+}
+

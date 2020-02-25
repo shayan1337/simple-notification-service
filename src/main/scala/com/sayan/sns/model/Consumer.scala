d06ff1c91e43a5158d8d19b25add1450dfa89fea
Sayan <shayan1337@gmail.com>
2020-02-25 18:58:46 +0530

    Add topic binding to consumers

--- src/main/scala/com/sayan/sns/model/Consumer.scala
+++ src/main/scala/com/sayan/sns/model/Consumer.scala
@@ -4,4 +4,4 @@ package com.sayan.sns.model
  * Represents a Consumer who is notified of the messages sent to @Topic
  */
 
-case class Consumer(serialVersionUID: Option[SerialVersionUID], name: String)
+case class Consumer(serialVersionUID: Option[SerialVersionUID], name: String, listeningTo : List[Topic])

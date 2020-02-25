483d3d55e1bba47a4c9a6bc9ec10bd5c6cd18493
Sayan <shayan1337@gmail.com>
2020-02-25 19:26:19 +0530

    Add failure class to map http status codes to service failures

---
+++ src/main/scala/com/sayan/sns/error/Failure.scala
@@ -0,0 +1,30 @@
+package com.sayan.sns.error
+
+import spray.http.{StatusCodes, StatusCode}
+
+/**
+ * Service failure description.
+ */
+case class Failure(message: String, errorType: FailureType.Value) {
+
+  def getStatusCode: StatusCode = {
+    FailureType.withName(this.errorType.toString) match {
+      case FailureType.BadRequest => StatusCodes.BadRequest
+      case FailureType.NotFound => StatusCodes.NotFound
+      case FailureType.Duplicate => StatusCodes.Forbidden
+      case FailureType.DatabaseFailure => StatusCodes.InternalServerError
+      case _ => StatusCodes.InternalServerError
+    }
+  }
+}
+
+object FailureType extends Enumeration {
+  type Failure = Value
+
+  val BadRequest = Value("bad_request")
+  val NotFound = Value("not_found")
+  val Duplicate = Value("entity_exists")
+  val DatabaseFailure = Value("database_error")
+  val InternalError = Value("internal_error")
+}
+

81195bbcad93e52fd5330dc0b6da70adce376934
Sayan <shayan1337@gmail.com>
2020-02-25 21:50:27 +0530

    Return object id for topic inserted

--- src/main/scala/com/sayan/sns/repository/TopicRepository.scala
+++ src/main/scala/com/sayan/sns/repository/TopicRepository.scala
@@ -1,11 +1,9 @@
 package com.sayan.sns.repository
 
-import com.sayan.sns.model.Topic
-
-import utils._
-
 import com.mongodb.casbah.MongoConnection
 import com.mongodb.casbah.commons.{Imports, MongoDBObject}
+import com.sayan.sns.model.Topic
+import utils._
 
 
 /**
@@ -13,10 +11,11 @@ import com.mongodb.casbah.commons.{Imports, MongoDBObject}
  */
 class TopicRepository(dbConnection: MongoConnection) {
 
-  def save(topic: Topic) = {
+  def save(topic: Topic): String = {
     val topicDao = buildDao(topic)
     val collection = dbConnection(Utils.DATABASE_NAME)(Utils.TOPIC_TABLE_NAME)
     collection.save(topicDao)
+    topicDao.get("_id").toString
   }
 
   private def buildDao(topic: Topic): Imports.DBObject = {

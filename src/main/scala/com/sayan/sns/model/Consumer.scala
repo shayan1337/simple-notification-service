0084e68baae4d6b2688889ae8ac2d32a931e4486
Sayan <shayan1337@gmail.com>
2020-02-25 18:55:58 +0530

    Add description to consumer class

--- src/main/scala/com/sayan/sns/model/Consumer.scala
+++ src/main/scala/com/sayan/sns/model/Consumer.scala
@@ -1,3 +1,7 @@
 package com.sayan.sns.model
 
+/**
+ * Represents a Consumer who is notified of the messages sent to @Topic
+ */
+
 case class Consumer(serialVersionUID: Option[SerialVersionUID], name: String)
